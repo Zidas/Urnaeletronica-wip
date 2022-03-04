fe4199686a719910a1623ebc830afd5db43458f7
Zidani <89753547+Zidas@users.noreply.github.com>
2022-03-04 04:00:17 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,77 @@
+let etapas= [
+    {
+            titulo: 'VEREADOR',
+            numeros: 5,
+            candidatos :[
+                    {
+
+                            numero:' 38111 ',
+                            nome: 'Fulano de tatata',
+                            partido: 'Sus',
+                            fotos:[
+                                {url:'istockphoto-523601319-1024x1024.jpg', legenda:'Vereador'}
+                            ]
+
+                    },
+                    {   
+                        numero:' 45555 ',
+                        nome: 'Silvano da Silva',
+                        partido: 'Fdp',
+                        fotos:[
+                            {url:'237965-descubra-como-dar-banho-em-idoso-de-maneira-correta-e-segura.jpg', legenda:'Vereador'}
+                        ]
+
+
+                    },
+
+            ]
+
+    },
+    {
+            titulo:'PREFEITO',
+            numeros: 2,
+            candidatos: [
+                {
+                    numero:' 12 ',
+                    nome: 'Candido de teresopolis',
+                    partido: 'FDTS',
+                    vice: 'lisbert',
+                    fotos:[
+                        {url:'IMGS_5705.jpg-scaled-e1609792544655.jpeg', legenda:'Prefeito'},
+                        {url:'237965-descubra-como-dar-banho-em-idoso-de-maneira-correta-e-segura.jpg', legenda:'Vice-Prefeito', small:true}
+                    ]
+
+
+                },
+                    {
+                        numero:' 44 ',
+                        nome: 'Luciano barbisa',
+                        partido: 'ZICA',
+                        vice: 'Lolinca',
+                        fotos:[
+                            {url:'istockphoto-523601319-1024x1024.jpg', legenda:'Prefeito'},
+                            {url:'237965-descubra-como-dar-banho-em-idoso-de-maneira-correta-e-segura.jpg', legenda:'Vice-Prefeito', small:true}
+                        ]
+    
+    
+                    }
+
+            ]
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
